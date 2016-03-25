dc51730d6d8f7248ad0e3d4e9647f1fba7933cbd
persaianbot <mrx2121mrx@gmail.com>
2016-03-25 19:16:21 +0430

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -5,7 +5,7 @@ function run(msg, matches)
               return "Only Sudoers Can Use it"       end
   local tex = matches[1]
   local sps = matches[2]
-  local sp = 'Sucessfully done'
+  local sp = ':-) '
 
 for i=1, tex, 1 do
 
