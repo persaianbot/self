c7eae10e8a12899dc3bad7066b478c26e1999b53
persaianbot <mrx2121mrx@gmail.com>
2016-03-26 04:59:05 -0800

    Update sticker.lua

--- plugins/sticker.lua
+++ plugins/sticker.lua
@@ -1,44 +1,38 @@
-local function download(msg, success, result, matches)
+local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'sticker/' .. msg.from.id .. '.webp'
+    local file = 'data/'..msg.from.id..'.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
-    send_large_msg(receiver, 'please send /make for create sticker\n@BeatBot_Team :))', ok_cb, false)
-    redis:del("file:jpeg")
+    send_document(get_receiver(msg), file, ok_cb, false)
+    redis:del("photo:sticker")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
 end
 local function run(msg,matches)
-
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
     if msg.media then
-        if msg.media.type == 'photo' and redis:get("file:jpeg") then
-          if redis:get("file:jpeg") == 'waiting' then
-            load_photo(msg.id, download, msg)
-          end
-        end
+      	if msg.media.type == 'photo' and redis:get("photo:sticker") then
+      		if redis:get("photo:sticker") == 'waiting' then
+        		load_photo(msg.id, tosticker, msg)
+      		end
+      	end
     end
-    if matches[1] == 'sticker' then
-      redis:set("file:jpeg", "waiting")
-      return 'Please send me photo now\n @Mosy15 :)) '
-end
-    if matches[1] == 'make' then
-            local receiver = get_receiver(msg)
-                  send_document(receiver, "./sticker/"..msg.from.id..".webp", ok_cb, false)
-
-      return 'by @Mosy15 :)) '
-end
-
-    return
+    if matches[1] == "tosticker" then
+    	redis:set("photo:sticker", "waiting")
+    	return 'Please send your photo now'
+    end
 end
 return {
   patterns = {
-    "^[/!](make)$",
-  "^[!/](sticker)$",
-  "%[(photo)%]",
+	"^[!/](tosticker)$",
+	"%[(photo)%]",
   },
   run = run,
 }
+
+
