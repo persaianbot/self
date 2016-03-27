59164ecd18f59cdadf1c9925efbb0209439de3af
persaianbot <mrx2121mrx@gmail.com>
2016-03-27 23:25:04 +0430

    Create info.lua

---
+++ plugins/info.lua
@@ -0,0 +1,13 @@
+do function run(msg, matches)
+  return "🔘Group id : "..msg.from.id.."\n🔘Group name : "..msg.to.title.."\n🔘full name : "..(msg.from.first_name or '---').."\n🔘first name : "..(msg.from.first_name or '---').."\n🔘last name : "..(msg.from.last_name or '---').."\n🔘your id : "..msg.from.id.."\n🔘username : @"..(msg.from.username or '---').."\n\n🔘phone number : +"..(msg.from.phone or '---')  
+end
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+    "^([Mm]yinfo)"
+
+},
+  run = run
+}
+end
