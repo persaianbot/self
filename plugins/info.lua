84a2830596fc283a16dbcc271dc73a24dab3daed
persaianbot <mrx2121mrx@gmail.com>
2016-03-27 23:30:22 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,13 +1,14 @@
 do function run(msg, matches)
-  return "🔘Group id : "..msg.from.id.."\n🔘Group name : "..msg.to.title.."\n🔘full name : "..(msg.from.first_name or '---').."\n🔘first name : "..(msg.from.first_name or '---').."\n🔘last name : "..(msg.from.last_name or '---').."\n🔘your id : "..msg.from.id.."\n🔘username : @"..(msg.from.username or '---').."\n\n🔘phone number : +"..(msg.from.phone or '---')  
+  return "😎Supergroup id : "..msg.from.id.."\n😎Supergroup name : "..msg.to.title.."\n😎full name : "..(msg.from.first_name or '---').."\n😎first name : "..(msg.from.first_name or '---').."\n😎last name : "..(msg.from.last_name or '---').."\n😎your id : "..msg.from.id.."\n😎username : @"..(msg.from.username or '---').."\n\n😎phone number : +"..(msg.from.phone or '---')  
 end
 return {
   description = "", 
   usage = "",
   patterns = {
-    "^([Mm]yinfo)"
+    "^[!/#][Mm]yinfo$",
+"^[!/#][Mm]y [Ii]nfo$",
 
-},
+  },
   run = run
 }
 end
