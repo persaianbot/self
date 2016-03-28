f4b24d9538c24d99a601321c5afce39b85b37363
persaianbot <mrx2121mrx@gmail.com>
2016-03-28 16:50:38 +0430

    Update hp.lua

--- plugins/hp.lua
+++ plugins/hp.lua
@@ -1,6 +1,6 @@
 local function run(msg, matches )
   if matches[1] == "Help>" then
-    return "قابلیت های من:\n\nInfo\n\nId\n\nuser\n\nCalc\n\n"
+    return "قابلیت های من:\n\n🔱Info: دریافت اطلاعات خود\n🆔Id: دریافت ای دی خود\n➰user: دریافت یوزر خود\n✖Clac: ماشین حساب\n🔱By: @Mosy15"
   end
 end
 
