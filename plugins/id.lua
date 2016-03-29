3be73d9542b17e0ae4e89ff0bf1c8f73446b018a
persaianbot <mrx2121mrx@gmail.com>
2016-03-29 23:42:09 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -2,6 +2,9 @@ local function run(msg, matches )
   if matches[1] == "Id" then
     return "‌‌"..msg.from.id
   end
+if matches[1] == "id" then
+    return "‌‌"..msg.from.id
+  end
 end
 return {
   patterns ={
